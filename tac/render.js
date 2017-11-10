6d935f417728ddb310d7851752978a52efe2fe71
alexlockhart7 <alex.lockhart@icfolson.com>
2017-11-10 06:21:47 -0500

    Using mustache partials

--- tac/render.js
+++ tac/render.js
@@ -1,42 +1,37 @@
 var fs = require('fs');
+var Mustache = require('Mustache');
+const { join } = require('path')
 
 var path = process.argv[2];
-
-console.log(path);
-
 var content = JSON.parse(fs.readFileSync('content.json', 'utf8'));
 
 path.split("/").forEach(function (val) {
   content = content[val];
 });
 
-console.log(content);
+var componentTemplates = {
+};
+
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+const componentDirectories = source =>
+  fs.readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+componentDirectories("components").forEach(function(directory) {
+    var componentName = directory.split("/").slice(-1)[0];
+    var componentTemplate = directory + "/" + componentName + ".html";
+    componentTemplates[componentName] = fs.readFileSync(componentTemplate, 'utf8');
+});
 
 var html = "<html>\n<head>\n</head>\n<body>";
 
 Object.keys(content["content"]).forEach(function(componentName) {
-    var component = content["content"][componentName];
+    var template = componentTemplates[componentName]
+    var metadata = content["content"][componentName];
 
-    var componentTemplate = fs.readFileSync('components/'+component.compType+'/'+component.compType+'.html' , 'utf8');
-
-    var componentHtml = generate(componentTemplate);
+    var componentHtml =  Mustache.render(template, metadata, componentTemplates);
 
     html += "\n" + componentHtml;
 });
-
-function generate(template) {
-    var html = "";
-
-    var str = "I learned to play the Ukulele in Lebanon."
-    var regex = /div/g, result, indices = [];
-
-    while ( (result = regex.exec(str)) ) {
-        console.log(result.index)
-        console.log(result)
-    }
-
-    return template;
-}
 
 html += "</body>\n</html>";
 
