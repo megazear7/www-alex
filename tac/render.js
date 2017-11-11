1eb25c5bc0fccb7989620b2d2e8117986ce13d27
alexlockhart7 <alex.lockhart@icfolson.com>
2017-11-11 08:51:43 -0500

    Added parent, children, and home attributes to the page attribute.

--- tac/render.js
+++ tac/render.js
@@ -4,12 +4,25 @@ const { join } = require('path')
 
 var render = function(path) {
     var page = JSON.parse(fs.readFileSync('content.json', 'utf8'));
+    var home = page;
+    var parent = page;
+    var pathParts = path.split("/");
 
     if (path.length > 0) {
-        path.split("/").forEach(function (val) {
-            console.log(val);
-          page = page[val];
+        pathParts.forEach(function (val) {
+            page = page[val];
         });
+
+        var parentPathParts = pathParts;
+        parentPathParts.pop();
+
+        if (parentPathParts.length > 0) {
+            parentPathParts.forEach(function (val) {
+                parent = page[val];
+            });
+        }
+    } else {
+        parent = { };
     }
 
     var componentTemplates = {};
@@ -37,6 +50,23 @@ var render = function(path) {
 
     render = function() {
         this.page = page;
+        this.page.home = home;
+        this.page.parent = parent;
+        this.page.siblings = [ ];
+        this.page.children = [ ];
+
+        for(var key in this.page) {
+            if (key != "parent" && this.page[key].tacType === "page") {
+                this.page.children.push(this.page[key]);
+            }
+        }
+
+        for(var key in this.page.parent) {
+            if (key != "parent" && this.page.parent[key].tacType === "page") {
+                this.page.siblings.push(this.page.parent[key]);
+            }
+        }
+
         this.render = render;
 
         return Mustache.render('{{> ' + this.compType  + '}}', this, componentTemplates);
