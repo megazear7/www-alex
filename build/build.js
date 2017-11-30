387113d83c17fc63a83b9c11ca90d27ff3380288
alexlockhart7 <alex.lockhart@icfolson.com>
2017-11-30 08:43:52 -0500

    Added updates to the compiled /build directory

--- build/build.js
+++ build/build.js
@@ -1,14 +1,10 @@
+// Whatever is exported here will be available in examples.html during
+// server side rendering underneath "model"
+
 const init = function(node) {
   return {
     example1: "HELLO - " + node.path + " - BYE",
     example2: "My parents path is: " + node.parent.path + " yay!"
-  }
-};
-
-module.exports = { init: init };
-const init = function(node) {
-  return {
-    example: "HELLO - " + node.path + " - BYE"
   }
 };
 
